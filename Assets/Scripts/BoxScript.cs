eb570f69cb2e2da8cff76baee5fb1fd53ada4944
GiraffeSummer <gidoselten@hotmail.com>
2018-09-06 18:37:53 +0200

    Does this work?

---
+++ Assets/Scripts/BoxScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxScript : MonoBehaviour
+{
+
+    public Item item;
+    public int hp;
+    ItemList il;
+
+    // Use this for initialization
+    void Start()
+    {
+        il = FindObjectOfType<ItemList>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < -5f)
+        {
+            Destroyed();
+        }
+    }
+
+    void Destroyed()
+    {
+        Destroy(GetComponent<Rigidbody>());
+        //iTween.rot(this.gameObject, Quaternion.identity.eulerAngles, 1f);
+
+        this.transform.rotation = Quaternion.identity;
+        this.transform.position = new Vector3(transform.position.x, il.Floor.transform.position.y + 1f, transform.position.z);
+        //iTween.MoveTo(this.gameObject, new Vector3(transform.position.x, il.Floor.transform.position.y + 1f, transform.position.z), 2f);
+
+        GameObject pan = Instantiate(il.panel, transform.position, Quaternion.identity, transform);
+        pan.GetComponent<Renderer>().material.mainTexture = item.image;
+        iTween.MoveTo(pan, new Vector3(transform.position.x, transform.position.y + 2f, transform.position.z), 2f);
+        iTween.RotateTo(pan, new Vector3(-90f, 0f, 0f), 1f);
+
+        Destroy(this.gameObject, 3f);
+    }
+
+    void TakeDamage(Vector3 velocity)
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Nut"))
+        {
+            TakeDamage(collision.relativeVelocity);
+        }
+    }
+}
---
+++ Assets/Scripts/CoconutManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoconutManager : MonoBehaviour
+{
+
+    public GameObject nut;
+    float power = 100f;
+    float cooldown = 1.5f;
+    bool canFire = true;
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0) && canFire)
+        {
+            GameObject n = Instantiate(nut, FindObjectOfType<Camera>().gameObject.transform.position, Quaternion.identity);
+            Rigidbody rb = n.GetComponent<Rigidbody>();
+            Debug.Log(Camera.main.ViewportToWorldPoint(Input.mousePosition));
+            rb.AddForce((-Camera.main.ViewportToWorldPoint(Input.mousePosition * 10)) * power);
+            canFire = false;
+            StartCoroutine(Cooldown(cooldown));
+            Destroy(n, 3f);
+
+        }
+    }
+
+    IEnumerator Cooldown(float down)
+    {
+        yield return new WaitForSeconds(down);
+        canFire = true;
+    }
+}
