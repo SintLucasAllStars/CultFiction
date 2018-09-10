fadbc40a59b66e22eeb95d5b22cb5b510fdcf6c2
Emiel van den Brink <emielvdbrink@gmail.com>
2018-09-10 12:12:36 +0200

    added basic phone routine

---
+++ The Overdose Game/Assets/Scripts/PhoneController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhoneController : MonoBehaviour, Iinteractable
+{
+    public bool pickedUpPhone;
+    public bool ringing;
+    private GameObject dialogBox;
+
+	void Start ()
+    {
+        pickedUpPhone = false;
+        ringing = false;
+        dialogBox = GameObject.Find("DialogBox");
+        dialogBox.SetActive(false);
+        StartCoroutine(PhoneCoroutine());
+	}
+
+    public void OnClick()
+    {
+        if (ringing)
+        {
+            pickedUpPhone = true;
+            dialogBox.SetActive(true);
+        }
+    }
+
+    public void CloseDialog(bool accepted)
+    {
+        pickedUpPhone = false;
+        dialogBox.SetActive(false);
+
+        if (accepted)
+        {
+            Debug.Log("sold some weed");
+        }
+        else
+        {
+            Debug.Log("denied weed");
+        }
+
+        StartCoroutine(PhoneCoroutine());
+    }
+
+    private void Ring()
+    {
+        Debug.Log("RING!");
+    }
+
+    private IEnumerator PhoneCoroutine()
+    {
+        yield return new WaitForSeconds(3f);
+
+        ringing = true;
+
+        while (!pickedUpPhone)
+        {
+            Ring();
+            yield return new WaitForSeconds(2f);
+        }
+
+        ringing = false;
+    }
+}
---
+++ The Overdose Game/Assets/Scripts/RayCaster.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCaster : MonoBehaviour
+{
+    private Camera cam;
+
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    void Update ()
+    {
+        RaycastHit hit;
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(ray.origin, ray.direction * 100, Color.red);
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.gameObject.tag == "Interactable")
+            {
+                if (Input.GetKey(KeyCode.Mouse0))
+                {
+                    hit.collider.GetComponent<Iinteractable>().OnClick();
+                }
+            }
+
+        }
+	}
+}
