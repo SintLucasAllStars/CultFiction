eed4b3b2a26580826dfb4501799d4b72881e81a8
rens224 <rensbrekel@gmail.com>
2018-09-10 20:36:26 +0200

    Add AR Scaling

---
+++ CultFiction-Rens/Assets/ChangeARScale.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeARScale : MonoBehaviour
+{
+
+    public GameObject arSessionOrigin;
+    public GameObject Content;
+    private Slider _slider;
+
+    private void Start()
+    {
+        _slider = GetComponent<Slider>();
+    }
+
+    public void ChangeScale(int value)
+    {
+        arSessionOrigin.transform.localScale = Vector3.one * _slider.value;
+        Content.transform.localScale = Vector3.one / _slider.value;
+    }
+}
