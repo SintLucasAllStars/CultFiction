47c2886fbf6ae9b36fa30fe790219ee3bc8e7ba2
Koen <koengroten@gmail.com>
2018-09-10 12:36:17 +0200

    Added models and movement scripts

---
+++ cultfiction2018/Assets/Scripts/HandMovement.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandMovement : MonoBehaviour
+{
+
+    
+    public float Speed = 10.0f;
+    public float RotationSpeed = 10.0f;
+    
+    public Vector2 Xclamp;
+    public Vector2 Yclamp;
+    
+    private int _boundary = 1;
+    private int _width;
+    private int _height;
+
+    private float _cameraOffsetX;
+    private float _cameraOffsetY;
+    private float _cameraOffsetZ;
+    
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _width = Screen.width;
+	    _height = Screen.height;
+        SetCameraOffsets();
+
+	}
+
+    private void SetCameraOffsets()
+    {
+        _cameraOffsetX = transform.position.x - Camera.main.transform.position.x;
+        _cameraOffsetY = Camera.main.transform.position.y - transform.position.y;
+        _cameraOffsetZ = transform.position.z - Camera.main.transform.position.z;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    if (Input.mousePosition.x > _width - _boundary)
+	    {
+	        transform.position -= Xvector();
+	    }
+
+	    if (Input.mousePosition.x < 0 + _boundary)
+	    {
+	        transform.position -= Xvector();
+	    }
+
+	    if (Input.mousePosition.y > _height - _boundary)
+	    {
+	        transform.position -= Yvector();
+	    }
+
+	    if (Input.mousePosition.y < 0 + _boundary)
+	    {
+	        transform.position -= Yvector();
+
+	    }
+
+	    transform.position = ClampVector();
+
+	    transform.eulerAngles = RotationEuler();
+        MoveCamera();
+
+	}
+
+    private void MoveCamera()
+    {
+        Camera.main.transform.position = new Vector3
+            (
+                transform.position.x - _cameraOffsetX,
+                transform.position.y + _cameraOffsetY,
+                transform.position.z - _cameraOffsetZ
+            );
+    }
+
+    private Vector3 RotationEuler()
+    {
+        return transform.eulerAngles + new Vector3(0, 0, ScrollWheelAxis() * Time.deltaTime * RotationSpeed);
+    }
+
+    private float ScrollWheelAxis()
+    {
+        return Input.GetAxis("Mouse ScrollWheel");
+    }
+    
+    
+
+    private Vector3 Yvector()
+    {
+        return new Vector3(Input.GetAxis("Mouse Y") * Time.deltaTime * Speed,0,0);
+    }
+
+    private Vector3 Xvector()
+    {
+        return new Vector3(0,0,Input.GetAxis("Mouse X") * Time.deltaTime * Speed);
+    }
+
+    private Vector3 ClampVector()
+    {
+        return new Vector3(Mathf.Clamp(transform.position.x,Xclamp.x,Xclamp.y),transform.position.y,Mathf.Clamp(transform.position.z,Yclamp.x,Yclamp.y));
+    }
+    
+}
---
+++ cultfiction2018/Assets/Scripts/Syringe.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+
+public class Syringe : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    private bool _hit;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        if (_rigidbody.useGravity && _hit)
+        {
+
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        _hit = false;;
+    }
+
+    // Update is called once per frame
+	void Update () 
+	{
+	    if (Input.GetMouseButtonDown(0))
+	    {
+	        transform.SetParent(null);
+	        _rigidbody.useGravity = true;
+	        _rigidbody.AddRelativeForce(0,2000,0);
+	    }
+	}
+}
