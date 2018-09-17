6c60a5932483b87e37a54b7e78e9fdd2785334b9
mosenpai <mo4vids@gmail.com>
2018-09-17 22:02:55 +0200

    Mohamed Ismael - Rhythm Bowling
    I based my game on the bowling scene from The Big Lebowski https://www.youtube.com/watch?v=GZR58d77a4A .
    
    I made a  bowling game where you get extra points based on the music. The higher the bowling pins the more points you get.

---
+++ CultFiction/CultFiction/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+
+    public List<GameObject> pins = new List<GameObject>();
+    public Text score;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void Clean()
+    {
+        Time.timeScale = 1;
+        pins.Clear();
+        score.text = "";
+    }
+
+    public void Tally()
+    {
+        int total = 0;
+        int bonusPoints = 0;
+        for (int i = 0; i < pins.Count; i++)
+        {
+            total++;
+            bonusPoints += Mathf.RoundToInt(pins[i].gameObject.transform.localScale.y - 1f);
+        }
+        score.text = "Score : " + total + "+" + bonusPoints;
+    }
+}
