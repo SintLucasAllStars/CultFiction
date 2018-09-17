818f180de1db0d96acd3f9beacc6f8556565ec42
Rick Rijkers <Rick.RIjkers1@gmail.com>
2018-09-17 21:46:48 +0200

    added game over screen

--- Assets/Scripts/Controller/GameController.cs
+++ Assets/Scripts/Controller/GameController.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class GameController : Singleton<GameController> {
 	public CharacterController player;
 	public int score;
+	public GameObject playerDiedWindow;
 	// Use this for initialization
 	void Start () {
-		
+		playerDiedWindow.SetActive(false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(player.dead){
+			playerDiedWindow.SetActive(true);
+		}
 	}
 }
