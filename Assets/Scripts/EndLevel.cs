0e9e8fc69ad6a0bc78b044cd2ee3105eba49ee36
Jeffrey van den Elshout <jeffreyvde15@gmail.com>
2018-09-17 20:04:42 +0200

    Fix end level did not check for round

--- Assets/Scripts/EndLevel.cs
+++ Assets/Scripts/EndLevel.cs
@@ -2,17 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EndLevel : MonoBehaviour, IInteractable {
+public class EndLevel : MonoBehaviour, IInteractable
+{
 
     [SerializeField] int scoreRequired;
     [SerializeField] int roundRequired;
 
     public void Interact(PlayerController controller)
     {
-        if(Gamemanager.instance.GetScore() >= scoreRequired)
+        if(Gamemanager.instance.GetScore() >= scoreRequired && Gamemanager.instance.GetRound() >= roundRequired)
             UnityEngine.SceneManagement.SceneManager.LoadScene(0);
-
-        Debug.Log("You broke");
+        else
+            Debug.Log("You broke");
     }
 
 }
