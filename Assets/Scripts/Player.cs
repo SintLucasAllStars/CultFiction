6b75f38e7a38d6217c854252dd72ed2e1268c29b
Jorrit <Jorritslaats@gmail.com>
2019-09-11 13:16:47 +0200

    Add spider and player script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Spider
+{
+    private float _xDir;
+    private float _zDir;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        _xDir = Input.GetAxisRaw("Horizontal");
+        _zDir = Input.GetAxisRaw("Vertical");
+
+        if (_xDir != 0 || _zDir != 0)
+        {
+            Walk();
+        }
+    }
+
+    protected override void Walk()
+    {
+        base.Walk();
+        Rotate();
+    }
+
+    private void Rotate()
+    {
+        Vector3 moveDir = Quaternion.LookRotation(new Vector3(_xDir, 0, _zDir), Vector3.up).eulerAngles;
+        Vector3 bodyDir = _body.transform.rotation.eulerAngles;
+
+        float newDir = Mathf.MoveTowardsAngle(bodyDir.y, moveDir.y - _body.transform.position.y, _turnSpeed * Time.deltaTime);
+        //Vector3 rotateDir = Vector3.RotateTowards(bodyDir, moveDir - _body.transform.position, _turnSpeed * Time.deltaTime, _turnSpeed);
+        //Vector3 newDir = Vector3.RotateTowards(bodyDir, moveDir - _body.transform.position, _turnSpeed * Time.deltaTime, _turnSpeed);
+        _body.transform.rotation = Quaternion.Euler(0, newDir, 0);
+    }
+}
---
+++ Assets/Scripts/Spider.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spider : MonoBehaviour
+{
+    [SerializeField]
+    protected float _turnSpeed;
+
+    [SerializeField]
+    protected GameObject _body;
+
+    [SerializeField]
+    private float _speed = 0.0f;
+
+    protected bool _isWalking = false;
+
+    protected float Speed { get => _speed; set => _speed = value; }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    protected virtual void Walk()
+    {
+        transform.Translate(_body.transform.forward * Time.deltaTime * Speed);
+    }
+}
