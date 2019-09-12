f3da8ae8125922a0938a6df48f69f1880c4def37
Mneijndorff <33960298+Mneijndorff99@users.noreply.github.com>
2019-09-12 14:07:05 +0100

    Golf Game
    added shooting functionality, power and direction

---
+++ CultFiction/Assets/Scripts/ShootGolfBall.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootGolfBall : MonoBehaviour
+{
+    public GameObject currentBall;
+    public GameObject spawnpoint;
+    public GameObject golfball;
+    Quaternion ClubOriginalRot;
+    public GameObject club;
+    public int multiplier;
+    public float maxStrength;
+    public float minStrength;
+    public float power;
+    Vector3 shootPos;
+    public GameObject RotatePoint;
+    public GameObject dirPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Respawn();
+        ClubOriginalRot = club.transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.tag == "Shootground")
+                {
+                    Debug.Log("Hit this point ");
+                    Debug.Log(hit.point);
+                    dirPos.transform.position = hit.point;
+                }
+                else
+                {
+                    Debug.Log("You have to choose a position on the shootground");
+                }
+            }
+        }
+        // Moves the club around but causes issues
+        if (Input.GetKey(KeyCode.A))
+        {
+            RotatePoint.transform.Rotate(new Vector3(0, 1), Space.World);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            RotatePoint.transform.Rotate(new Vector3(0, -1), Space.World);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            float rot = -1;
+            if(power >= maxStrength)
+            {
+                power = maxStrength;
+                rot = 0;
+            }
+            else
+            {
+                power += multiplier * Time.deltaTime;
+            }
+            club.transform.Rotate(new Vector3(rot, 0), Space.Self);
+            //orPos = club.transform.position;
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            //club.transform.position = orPos;
+            club.transform.rotation = ClubOriginalRot;
+            Shoot(power);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Respawn();
+        }
+    }
+
+    public void Shoot(float strenght)
+    {
+        Vector3 dir;
+        dir = currentBall.transform.position - dirPos.transform.position;
+        dir.Normalize();
+        currentBall.GetComponent<Rigidbody>().AddForce(dir * strenght, ForceMode.Impulse);
+        power = 0;
+        Destroy(currentBall, 5);
+        currentBall = null;
+        Respawn();
+    }
+
+    public void Respawn()
+    {
+        
+        if (currentBall == null)
+            currentBall = Instantiate(golfball, spawnpoint.transform.position, Quaternion.identity);
+        else
+            Debug.Log("There is already a ball noob shoot this one first");
+    }
+}
---
+++ CultFiction/Assets/pipe_pack/readme.txt
@@ -0,0 +1,43 @@
+Thank you for purchasing the Pipe Pack!
+
+There are 3 types of pipe :
+
+Pipes with caps on BOTH ends
+Pipes With one cap on one end
+Pipes with no caps
+
+Each of these pipe types has 4 sizes:
+
+Small
+Medium
+Large
+Huge
+
+ 
+
+Each is named accordingly, for example : p_medium_2_cap, simply means a medium sized pipe with 2 caps. 
+
+
+
+-3 valve meshes are included, each with their own clean, worn and rusty textures.
+
+-There are also 4 valve wheels, Red, Green, Blue, Rusty
+
+
+-Each pipe is assigned by default the 'clean' texture. There are materials for 'Worn' and 'Rusty' pipes, which are easily applied to the pipes. 
+ Each support also has these textures
+ You can also change the color of the pipes changing the 'Main Color' value in the inspector view. 
+
+- Each pipe texture has a normal map applied along with a specular map in the alpha channel of the .TIFF file.
+
+-The pipes work best when rotated in angles of 30 degrees, this way the caps connect well with each other
+
+-Connecting pipes is very easy with Unitys vertex snapping tool
+
+-Source files in .max format are included in a .rar file, should you want to edit or make new pipes ! 
+
+
+-There is a demo scene included to give you an idea of how they piece together
+
+
+If you have any questions or need assistance, dont hesitate to email me : willbullen@gmail.com
