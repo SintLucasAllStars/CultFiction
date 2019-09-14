82c31738be32ad05f5848ee52b380340c8fe04df
ertugrul013 <ertugrul013@hotmail.com>
2019-09-14 19:01:36 +0200

    :sparkles: patient class

---
+++ CultFiction/Assets/Scripts/patient.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class patient
+{
+    public GameObject person;
+    public float raduis;
+    public GameObject raduisObject;
+
+    public static float[] positions = new float[2] { 10, 30 };
+
+    public patient(GameObject _person, float _raduis)
+    {
+        this.person = _person;
+        this.raduis = _raduis;
+        this.raduisObject = person.transform.GetChild(0).gameObject;
+    }
+
+    public void setRaduis()
+    {
+        float size = raduis / 2;
+        raduisObject.transform.localScale = new Vector3(size, size, size);
+    }
+}
