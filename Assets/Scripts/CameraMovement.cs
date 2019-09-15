2aff9a19faef3ae40cfa0cc220b51c8668420d5d
Jorrit <Jorritslaats@gmail.com>
2019-09-15 17:40:08 +0200

    Adjust camera movement to be less chaotic

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -8,7 +8,13 @@ public class CameraMovement : MonoBehaviour
     private float _lerpSpeed = 0.0f;
 
     [SerializeField]
+    private float _xOffset = 0.0f;
+
+    [SerializeField]
     private float _yOffset = 0.0f;
+
+    [SerializeField]
+    private float _zOffset = 0.0f;
 
     private GameObject _target;
 
@@ -21,8 +27,7 @@ public class CameraMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector3 targetMovementPos = new Vector3(_target.transform.position.x, _target.transform.position.y + _yOffset, _target.transform.position.z);
+        Vector3 targetMovementPos = new Vector3(_target.transform.position.x + _xOffset, _target.transform.position.y + _yOffset, _target.transform.position.z + _zOffset);
         transform.position = Vector3.MoveTowards(transform.position, targetMovementPos, _lerpSpeed * Time.deltaTime);
-        transform.LookAt(_target.transform);
     }
 }
