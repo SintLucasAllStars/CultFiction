5b4133f9c2a3532ff6f876ee0f7d4962fc822af1
Jorrit <Jorritslaats@gmail.com>
2019-09-16 12:30:10 +0200

    Add bowling balls and a ball spawner

---
+++ Assets/Scripts/BallSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _bowlingBall;
+
+    [SerializeField]
+    private List<Material> _materials = new List<Material>();
+
+    [SerializeField]
+    private float _spawnIntervalMin = 1.0f;
+
+    [SerializeField]
+    private float _spawnIntervalMax = 1.0f;
+
+    private void Start()
+    {
+        StartCoroutine(SpawnBall());
+    }
+
+    private IEnumerator SpawnBall()
+    {
+        yield return new WaitForSeconds(Random.Range(_spawnIntervalMax, _spawnIntervalMax));
+        Instantiate(_bowlingBall, transform.position, Quaternion.identity);
+        StartCoroutine(SpawnBall());
+    }
+}
---
+++ Assets/Scripts/BowlingBall.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BowlingBall : MonoBehaviour
+{
+    [SerializeField]
+    private float _launchForce = 1.0f;
+
+    [SerializeField]
+    private float _launchDelay = 1.0f;
+
+    [SerializeField]
+    private float _maxOffset = 1f;
+
+    [SerializeField]
+    private List<Material> _materials = new List<Material>();
+
+    private Rigidbody _rigidbody;
+
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+
+        ChooseRandomColor();
+
+        StartCoroutine(WaitForLaunch());
+    }
+
+    private void ChooseRandomColor()
+    {
+        GetComponentInChildren<MeshRenderer>().material = _materials[Random.Range(0, _materials.Count)];
+    }
+
+    private void LaunchBall()
+    {
+        Vector3 dir = new Vector3(-_launchForce, 0, Random.Range(-_maxOffset, _maxOffset));
+        _rigidbody.AddForce(dir, ForceMode.Impulse);
+    }
+
+    private IEnumerator WaitForLaunch()
+    {
+        yield return new WaitForSeconds(_launchDelay);
+        LaunchBall();
+    }
+}
