1ab7cd2bf70e18cd0e1303c411b1e835560ea33f
Jorrit <Jorritslaats@gmail.com>
2019-09-18 10:33:42 +0200

    Add game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance = null;
+
+    private int _currentSpiderHighscore = 0;
+
+    private float _currentTime = 0;
+
+    private bool _gameIsRunning = false;
+
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+            return;
+        }
+        else
+        {
+            Instance = this;
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Update()
+    {
+        if (_gameIsRunning)
+        {
+            _currentTime += Time.deltaTime;
+        }
+    }
+}
