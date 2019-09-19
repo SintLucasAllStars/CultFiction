ebf39a94a1398e4a86289bf673215b850104214e
Jorrit <Jorritslaats@gmail.com>
2019-09-19 15:57:55 +0200

    Respawn pins after they got hit

---
+++ Assets/Scripts/BowlingPin.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BowlingPin : MonoBehaviour
+{
+    private PinSpawner _pinSpawner;
+
+    private void Start()
+    {
+        _pinSpawner = GameObject.FindObjectOfType<PinSpawner>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<BowlingBall>())
+        {
+            _pinSpawner.SendPinSpawnSignal();
+        }
+    }
+}
---
+++ Assets/Scripts/PinSpawner.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PinSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _pins;
+
+    [SerializeField]
+    private GameObject _currentPins;
+
+    [SerializeField]
+    private float _fallDelay = 10.0f;
+
+    [SerializeField]
+    private float _replaceDelay = 2.0f;
+
+    private bool _signalReceived = false;
+
+    public void SendPinSpawnSignal()
+    {
+        if (!_signalReceived)
+        {
+            _signalReceived = true;
+            Invoke("RemovePinCollisions", _fallDelay);
+            Invoke("SwapOutNewPins", _fallDelay + _replaceDelay);
+        }
+    }
+
+    private void RemovePinCollisions()
+    {
+        foreach (BoxCollider collider in _currentPins.GetComponentsInChildren<BoxCollider>())
+        {
+            Destroy(collider);
+        }
+
+        foreach (CapsuleCollider collider in _currentPins.GetComponentsInChildren<CapsuleCollider>())
+        {
+            Destroy(collider);
+        }
+    }
+
+    private void SwapOutNewPins()
+    {
+        Destroy(_currentPins);
+        _currentPins = Instantiate(_pins, transform.position, transform.rotation);
+        _signalReceived = false;
+    }
+}
