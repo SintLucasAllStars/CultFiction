5f2dd1272555570f8781dc5b94f8710e6504414e
ertugrul013 <ertugrul013@hotmail.com>
2019-09-20 15:04:46 +0200

    :art: added extra wait function in a separte class

---
+++ CultFiction/Assets/Scripts/extendedFunctions.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System;
+using System.Collections;
+
+public class extendedFunctions : MonoBehaviour
+{
+    /// <summary>
+    /// waits for the given time
+    /// </summary>
+    /// <param name="wait">amount of time to wait</param>
+    /// <param name="action">callback</param>
+    public void Wait(float wait, Action action)
+    {
+        StartCoroutine(_wait(wait, action));
+    }
+
+    IEnumerator _wait(float wait, Action callback)
+    {
+        yield return new WaitForSeconds(wait);
+        callback();
+    }
+}
