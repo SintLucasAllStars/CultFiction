153aed3e9107e75fed363a10994c0753c6c1647c
ertugrul013 <ertugrul013@hotmail.com>
2019-09-20 15:07:35 +0200

    :recycle: a different spawnpoint will from now on be used for spawning the raycast

--- CultFiction/Assets/Scripts/playerController.cs
+++ CultFiction/Assets/Scripts/playerController.cs
@@ -1,40 +1,75 @@
+using System;
 using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
+    private Transform shootPoint;
+    private int mistakes;
+    public int Mistakes
+    {
+        get
+        {
+            return mistakes;
+        }
+        set
+        {
+            if (value > 3)
+            {
+                NoLivesLeft();
+            }
+            else
+            {
+                mistakes = value;
+            }
+        }
+    }
 
     public Animation[] animations;
 
+
+    void Start()
+    {
+        shootPoint = this.transform.GetChild(0).transform;
+    }
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
-        {
             fireRay();
-        }
     }
 
     void fireRay()
     {
-        Ray myray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
+        Ray myray = new Ray(shootPoint.position, shootPoint.transform.position);
         RaycastHit hit;
+
+        Debug.DrawRay(shootPoint.position, shootPoint.transform.position, Color.blue);
 
         if (Physics.Raycast(myray, out hit, Mathf.Infinity))
         {
             if (checkTag(hit.transform.tag, "hitarea"))
             {
-                Debug.Log("hitarea: has been hit");
-                // PlayAnimation(animations[0]);
+                gameController.instance.PatientHit();
+                return;
             }
-            return;
-        }
+            else if (checkTag(hit.transform.tag, "phone"))
+            {
+                // gameController.instance call clip
+                return;
+            }
 
+        }
     }
 
     bool checkTag(string objectTag, string tag)
     {
         bool isHit;
         return isHit = objectTag == tag ? true : false;
+    }
+
+    private void NoLivesLeft()
+    {
+        //using it like this so that I don't need to imported over the whole schript
+        UnityEngine.SceneManagement.SceneManager.LoadScene(2);
     }
 
     void PlayAnimation(Animation anim)
