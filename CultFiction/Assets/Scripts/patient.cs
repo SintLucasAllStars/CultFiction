76a5c68a0045cee4417d696358d2eaa88966efdd
ertugrul013 <ertugrul013@hotmail.com>
2019-09-20 15:05:22 +0200

    :art: basis for the patient class with its functions

--- CultFiction/Assets/Scripts/patient.cs
+++ CultFiction/Assets/Scripts/patient.cs
@@ -1,23 +1,29 @@
 ﻿using UnityEngine;
 
+[System.Serializable]
 public class patient
 {
     public GameObject person;
     public float raduis;
     public GameObject raduisObject;
 
-    public static float[] positions = new float[2] { 10, 30 };
+    public Vector3[] positions = new Vector3[2] {
+        new Vector3(0, 0, 0),
+        new Vector3(-16.04f, 0, -0.65f)
+    };
 
-    public patient(GameObject _person, float _raduis)
+    public patient(GameObject _person, GameObject _raduisObject, float _raduis)
     {
         this.person = _person;
         this.raduis = _raduis;
-        this.raduisObject = person.transform.GetChild(0).gameObject;
+        this.raduisObject = _raduisObject;
+        setRaduis();
     }
 
     public void setRaduis()
     {
         float size = raduis / 2;
-        raduisObject.transform.localScale = new Vector3(size, size, size);
+        Debug.Log(size);
+        raduisObject.transform.localScale = new Vector3(size, 0.1f, size);
     }
 }
