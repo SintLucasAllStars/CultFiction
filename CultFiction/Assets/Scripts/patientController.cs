3997e9ba7d6827c8cbdc3e1ecaec9b0336a4825f
ertugrul013 <ertugrul013@hotmail.com>
2019-09-20 15:10:51 +0200

    :beers: controls the basic movement of playercontoller and al its interactions

---
+++ CultFiction/Assets/Scripts/patientController.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class patientController : extendedFunctions
+{
+    [SerializeField]
+    private enum State
+    {
+        spawned,
+        onTheTable,
+        lived,
+        died
+    }
+
+    [SerializeField] private State state;
+
+    public GameObject raduisObject;
+    public float raduisOption;
+    public patient _patient;
+
+    /// <summary>
+    /// 0 = comming in
+    /// 1 = dead or alive
+    /// </summary>
+    public int target = 0;
+
+    private float speed = 0.15f;
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(this.transform.position, _patient.positions[target], speed);
+    }
+
+    public void InitPatient()
+    {
+        _patient = new patient(this.gameObject, raduisObject, Random.Range(2, 4));
+        state = State.spawned;
+        Wait(5f, () => { state = State.onTheTable; });
+        Debug.Log(state);
+    }
+
+    public void RemovePatient()
+    {
+        state = State.lived;
+        target = 1;
+        Wait(5f, () =>
+        {
+            gameController.instance.SpawnPatient();
+            Destroy(this.gameObject);
+        });
+    }
+}
