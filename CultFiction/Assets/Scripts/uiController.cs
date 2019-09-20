7514bc4514a9c8ac687d17cd1ef1e4575eab92ce
ertugrul013 <ertugrul013@hotmail.com>
2019-09-20 15:06:16 +0200

    :art: Finished work on the uicontroller

---
+++ CultFiction/Assets/Scripts/uiController.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using TMPro;
+using System;
+
+public class uiController : MonoBehaviour
+{
+    #region Singleton
+    public static uiController instance;
+    void Awake()
+    {
+        if (instance == null) instance = this;
+        else Destroy(this.gameObject);
+    }
+    #endregion
+
+    private bool isPaused;
+
+
+    [SerializeField] private TextMeshProUGUI scoreText;
+    public void PauseGame()
+    {
+        if (isPaused)
+        {
+            Time.timeScale = 1;
+            return;
+        }
+        Time.timeScale = 0;
+        isPaused = !isPaused;
+    }
+
+    public void ShowScore(int score)
+    {
+        scoreText.text = "score: " + score;
+    }
+
+    void UpdateMistakes()
+    {
+
+    } 
+}
