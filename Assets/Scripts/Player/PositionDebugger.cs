b02d1a0199f1af32138c32008076d34ff45ae90c
Mathijs van Dinther <ma.van.dinther@sintlucasedu.nl>
2020-05-12 12:28:52 +0200

    Added Debug Position

---
+++ Assets/Scripts/Player/PositionDebugger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PositionDebugger : MonoBehaviour
+{
+    private Text height;
+    private readonly float yPos;
+            
+    private Text position;
+    private readonly float xPos;
+    private readonly float zPos;
+
+    private void Awake()
+    {
+        height = GameObject.Find("Y").GetComponent<Text>();
+        position = GameObject.Find("X, Z").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DebugPosition(height, position);
+    }
+
+    void DebugPosition(Text y, Text xz)
+    {
+        y.text = transform.position.y.ToString();
+        xz.text = transform.position.x.ToString() + ", " + transform.position.z.ToString();
+    }
+}
