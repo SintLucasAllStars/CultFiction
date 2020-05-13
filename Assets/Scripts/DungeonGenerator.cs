725530c42e44750d9347804ed819d286614fc4d0
Ad Bressers <gamerad94@gmail.com>
2020-05-13 19:21:01 +0200

    Add maze algorithm
    Add procedural maze generator using growth algorithm.

--- Assets/Scripts/DungeonGenerator.cs
+++ Assets/Scripts/DungeonGenerator.cs
@@ -14,7 +14,12 @@ public class DungeonGenerator : MonoBehaviour
 
         public int numRoomTries;
         public int roomExtraSize = 0;
+
+        [Range(0, 1)]
+        public float mazeWindingPercent = 0.4f;
     }
+
+    public Tile[,] tiles;
 
     List<Rect> rooms = new List<Rect>();
 
@@ -27,7 +32,10 @@ public class DungeonGenerator : MonoBehaviour
 
     private void Start()
     {
+        tiles = new Tile[dungeon.width + 1, dungeon.height + 1];
+
         StartCoroutine(AddRooms());
+        //StartCoroutine(CreateMaze());
     }
 
     private IEnumerator AddRooms()
@@ -75,8 +83,87 @@ public class DungeonGenerator : MonoBehaviour
                 for (int j = y; j < (y + height); j++)
                 {
                     Vector2 pos = new Vector2(i, j);
-                    Instantiate(prefab, pos, Quaternion.identity);
-                    yield return new WaitForSeconds(.00001f);
+                    GameObject go = Instantiate(prefab, pos, Quaternion.identity);
+                    tiles[i, j].SetTile(go);
+                    yield return new WaitForEndOfFrame();
+                }
+            }
+        }
+        StartCoroutine(CreateMaze());
+    }
+
+    private IEnumerator CreateMaze()
+    {
+        for (int y = 1; y < dungeon.height; y += 2)
+        {
+            for (int x = 1; x < dungeon.width; x += 2)
+            {
+                if (tiles[x, y].HasTile()) //if position already has a tile
+                    continue;
+
+                Vector2 pos = new Vector2(x, y);
+
+                List<Vector2> cells = new List<Vector2>();
+                Vector2 lastDir = Vector2.zero;
+
+                StartRegion();
+                GameObject go = Instantiate(prefab, pos, Quaternion.identity);
+                tiles[(int)pos.x, (int)pos.y].SetTile(go);
+                cells.Add(pos);
+
+                while(cells.Count != 0)
+                {
+                    Vector2 cell = cells[cells.Count - 1];
+
+                    List<Vector2> unmadeCells = new List<Vector2>();
+
+                    List<Vector2Int> neighbours = new List<Vector2Int>();
+                    neighbours.Add(new Vector2Int((int)cell.x + 1, (int)cell.y));
+                    neighbours.Add(new Vector2Int((int)cell.x, (int)cell.y - 1));
+                    neighbours.Add(new Vector2Int((int)cell.x, (int)cell.y + 1));
+                    neighbours.Add(new Vector2Int((int)cell.x - 1, (int)cell.y));
+
+                    foreach(Vector2Int neighbour in neighbours)
+                    {
+                        if (neighbour.x >= 1 && neighbour.x < dungeon.width - 1 &&
+                            neighbour.y >= 1 && neighbour.y < dungeon.height - 1 &&
+                            !tiles[neighbour.x, neighbour.y].HasTile())
+                        {
+                            Vector2 dir = neighbour - cell;
+                            if (!tiles[(neighbour.x + (int)dir.x), (neighbour.y + (int)dir.y)].HasTile())
+                                unmadeCells.Add(dir);
+                        }
+                    }
+
+                    if (unmadeCells.Count != 0)
+                    {
+                        Vector2 dir;
+
+                        if (unmadeCells.Contains(lastDir) && Random.value >= dungeon.mazeWindingPercent)
+                        {
+                            dir = lastDir;
+                        }
+                        else
+                        {
+                            dir = unmadeCells[Random.Range(0, unmadeCells.Count)];
+                        }
+
+                        go = Instantiate(prefab, cell + dir, Quaternion.identity);
+                        tiles[(int)(cell.x + dir.x), (int)(cell.y + dir.y)].SetTile(go);
+
+                        go = Instantiate(prefab, cell + (dir * 2), Quaternion.identity);
+                        tiles[(int)(cell.x + (dir.x * 2)), (int)(cell.y + (dir.y * 2))].SetTile(go);
+
+                        cells.Add(cell + (dir * 2));
+                        lastDir = dir;
+                    }
+                    else
+                    {
+                        cells.RemoveAt(cells.Count - 1);
+                        lastDir = Vector2.zero;
+                    }
+                    //yield return new WaitForEndOfFrame();
+                    yield return new WaitForSeconds(0.0000000001f);
                 }
             }
         }
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct Tile
+{
+    public GameObject tile;
+    public Vector2 pos;
+
+    public bool HasTile()
+    {
+        if(tile != null)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public void SetTile(GameObject go)
+    {
+        tile = go;
+        pos = go.transform.position;
+    }
+}
