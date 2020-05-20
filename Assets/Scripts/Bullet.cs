ec06d5b6804b2b960b3b2eefb8ba613020783a6e
Paultje <p.ten.winkel@sintlucasedu.nl>
2020-05-20 14:12:31 +0200

    Shooting Mechanic + Pincushion mechanic

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    private bool m_Active;
+    [SerializeField] private int m_Power;
+    [SerializeField] private int m_Speed;
+    [SerializeField] private float m_MaxDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Active = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(m_Active == true)
+        {
+            transform.Translate(Vector3.up * (m_Speed * 10) * Time.deltaTime);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        transform.parent = other.gameObject.transform;
+        m_Active = false;
+        Rigidbody rb = other.gameObject.GetComponent<Rigidbody>();
+        Collider bc = gameObject.GetComponent<Collider>();
+        if (rb != null && bc != null)
+        {
+            rb.AddForce(Vector3.up * m_Power * 100);
+            Destroy(bc);
+        }
+    }
+
+    private void OnBecameInvisible()
+    {
+        Transform other = GameObject.FindWithTag("Player").transform;
+        if (m_Active == false || Vector3.Distance(other.position, transform.position) > m_MaxDistance)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+
+    [SerializeField] private GameObject m_Bullet;
+    [SerializeField] private Transform m_SpawnLoc;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetButtonUp("Fire1"))
+        {
+            Vector3 r = new Vector3(transform.eulerAngles.x + 90, transform.eulerAngles.y, 0);
+            
+            Instantiate(m_Bullet, m_SpawnLoc.position, Quaternion.Euler(r));
+        }
+        
+    }
+}
