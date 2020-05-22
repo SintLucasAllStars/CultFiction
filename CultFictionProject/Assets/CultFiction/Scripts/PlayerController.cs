4d7415e7b5a9f71672e1b7acb9b069da9d6b3de3
Quinn McGregor <quinn.mcgregor@hotmail.com>
2020-05-22 12:44:41 +0200

    Added Active Ragdoll Character And Basic Weapon

---
+++ CultFictionProject/Assets/CultFiction/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb = null;
+    private bool gunEquipt = true;
+
+    [SerializeField]
+    private float speed = 0;
+    [SerializeField]
+    private Gun gun = null;
+    [SerializeField]
+    private GameObject Hand = null;
+    [SerializeField]
+    private GameObject hips = null;
+    [SerializeField]
+    private Transform GunLoc= null;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+       
+    }
+
+    private void Update()
+    {
+        if (gunEquipt && Input.GetKeyDown(KeyCode.Space))
+        {
+            Hand.GetComponent<Rigidbody>().AddForce(transform.up + -transform.right * 100);
+            gun.Fire();
+        }
+    }
+
+    void Move()
+    {
+        float hAxis = Input.GetAxis("Horizontal");
+        float vAxis = Input.GetAxis("Vertical");
+
+        rb.AddForce(hips.transform.right * vAxis * speed);
+        transform.eulerAngles += transform.up * hAxis * 2;
+
+    }
+
+}
---
+++ CultFictionProject/Assets/CultFiction/Scripts/Weapons/Gun.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+
+public class Gun : MonoBehaviour
+{
+    [SerializeField]
+   
+
+    public VisualEffect Muzzle;
+
+    [SerializeField]
+    private GameObject Bullet;
+    [SerializeField]
+    private GameObject MuzzleLoc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Fire()
+    {
+        var go = Instantiate(Bullet, MuzzleLoc.transform.position, Quaternion.identity);
+        go.GetComponent<Rigidbody>().AddForce(transform.right * 2000);
+        Muzzle.Play();
+    }
+}
