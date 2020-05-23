f306c46e4fb21914fe965ef65fe9b43cb4cbddb8
Ad Bressers <gamerad94@gmail.com>
2020-05-23 16:42:04 +0200

    Add player rotation and shooting
    - player looks at mouse using animation blendtree
    - add shooting

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,16 +6,22 @@ public class Player : MonoBehaviour
 {
     public Camera cam;
     private Rigidbody2D rb;
+    private Animator anim;
+    public GameObject bullet;
 
     public float speed = 3.5f;
     private float cameraFollowSpeed = 5.0f;
 
     private float horizontal, vertical;
     private Vector2 moveDir;
+    private Vector2 mousePos;
+    private Vector2 lookDir;
+    private float angle;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
@@ -24,8 +30,17 @@ public class Player : MonoBehaviour
         vertical = Input.GetAxisRaw("Vertical");
 
         moveDir = new Vector2(horizontal, vertical) * speed;
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
 
-        //transform.Translate(moveDir); //Speed = 6f
+        lookDir = mousePos - rb.position;
+        angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+
+        Rotate();
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Shoot();
+        }
     }
 
     private void FixedUpdate()
@@ -33,9 +48,72 @@ public class Player : MonoBehaviour
         rb.velocity = moveDir;
     }
 
+    private void Rotate()
+    {
+        int parameterValue = GetRotation(angle);
+        anim.SetFloat("lookDir", parameterValue);
+    }
+
+    private void Shoot()
+    {
+        Vector2 firePos = rb.position + (lookDir.normalized * 0.4f);
+
+        GameObject go = Instantiate(bullet, firePos, Quaternion.Euler(0, 0, angle));
+        go.GetComponent<Rigidbody2D>().AddForce(lookDir * 5f, ForceMode2D.Impulse);
+    }
+
     private void LateUpdate()
     {
         Vector3 camOffset = transform.position + (-Vector3.forward * 5.0f);
         cam.transform.position = Vector3.Lerp(cam.transform.position, camOffset, Time.deltaTime * cameraFollowSpeed);
+    }
+
+    private int GetRotation(float angle)
+    {
+        if (angle < 23 && angle >= -23)
+        {
+            //North
+            return 1;
+        }
+        else if (angle < -23 && angle >= -68)
+        {
+            //North East
+            return 2;
+        }
+        else if (angle < -68 && angle >= -113)
+        {
+            //East
+            return 3;
+        }
+        else if (angle < -113 && angle >= -158)
+        {
+            //South East
+            return 4;
+        }
+        else if (angle < -158 && angle >= -203)
+        {
+            //South
+            return 5;
+        }
+        else if (angle < -203 && angle >= -248)
+        {
+            //South West
+            return 6;
+        }
+        else if (angle < -248 || angle >= 68)
+        {
+            //West
+            return 7;
+        }
+        else if (angle < 68 && angle >= 23)
+        {
+            //North West
+            return 8;
+        }
+        else
+        {
+            Debug.LogError("Angle: " + angle + " none of the directions compatible!!");
+            return 1;
+        }
     }
 }
