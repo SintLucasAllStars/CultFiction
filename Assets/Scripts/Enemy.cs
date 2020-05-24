74c6a83aa4a75f6aaec001094ed3a0e3ba34b99f
Ad Bressers <gamerad94@gmail.com>
2020-05-24 21:17:22 +0200

    Add enemy set-up
    -Started enemy script
    -need guns from other branch merch needed

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health;
+    public State state = State.Idle;
+    private float speed;
+    private float minAttackDistance, maxAttackDistance;
+    private float extraReloadDistance;
+    private Vector2 playerLostPos;
+
+    private void Update()
+    {
+        
+    }
+
+    private void ChangeState(State newState)
+    {
+        state = newState;
+    }
+}
+
+public enum State { Idle, Attack, Search, Dead }
