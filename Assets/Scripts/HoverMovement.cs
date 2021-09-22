ed96f24d853a0627c6ab2fdf8d1c1a74f6131b05
Rens <eekhoorn2001@gmail.com>
2021-09-22 15:50:15 +0200

    Imported project
    Had on different branch, let's continue from here.

---
+++ Assets/Scripts/HoverMovement.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    public float turnSpeed;
+
+    public float hoverDist;
+    public float drag;
+    public float gravity;
+    public bool isGrounded;
+    public float groundDist;
+
+    public Vector3 velocity;
+    private Vector2 pMoveInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = groundCheck();
+        GetMoveInput();
+        HandleGravity();
+    }
+
+    void HandleGravity()
+    {
+        velocity.y += gravity * Time.deltaTime;
+        if (groundCheck() && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+    }
+
+    private void GetMoveInput()
+    {
+        pMoveInput.x = Input.GetAxisRaw("Horizontal");
+        pMoveInput.y = Input.GetAxisRaw("Vertical");
+        pMoveInput = Vector3.ClampMagnitude(pMoveInput, 1);
+    }
+
+    private bool groundCheck()
+    {
+        RaycastHit hit;
+
+        Vector3 player = transform.position;
+
+        if (Physics.SphereCast(player, player.y / 2, Vector3.down, out hit, groundDist))
+        {
+            return true;
+        }
+        else { return false; }
+    }
+
+}
