b485bae669541eb514a4f6353f73b72c6a4c0b8b
esg <jelle.bekkers@live.nl>
2021-09-27 10:31:52 +0200

    Added movement
    + Added PlayerMovement
    + Added CameraMovement
    + Created test area

---
+++ WhipGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    private int speed = 10;
+
+    private Vector3 velocity;
+    private float gravity = -20.3f;
+
+    public Transform groundCheck;
+    private float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    private bool isGrounded;
+    public float jumpHeight = 3f;
+
+    void Update()
+    {
+        //MOVEMENT
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        //RUN
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = 15;
+        }
+        else { speed = 10; }
+
+        //VELOCITY
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        //JUMP
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+    }
+}
