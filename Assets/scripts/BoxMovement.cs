dccf7455c7b65d64bd5d6051d2d98324dff80715
Azathoth <wandererbetweenplanes@protonmail.com>
2021-09-29 13:01:58 +0200

    Added gamemanager

--- Assets/scripts/BoxMovement.cs
+++ Assets/scripts/BoxMovement.cs
@@ -1,27 +1,29 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class BoxMovement : MonoBehaviour {
-    // Flap mechanics based on rigid body
     private Rigidbody2D rb;
+    public GameManger gm;
     public Vector2 force = new Vector2(0, 10);
 
     private void Start() {
         rb = GetComponent<Rigidbody2D>();
-        //Flap();
+        gm = FindObjectOfType<GameManger>();
     }
     
     private void Update() {
-        //flap detection
         if (Input.GetButtonDown("Jump")) {
             Flap();
         }
+        gm.ScoreIncrement();
     }
 
     private void Flap() {
         rb.velocity = Vector2.zero;
         rb.AddForce(force);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.CompareTag("PipeSection"))
+            gm.DeathState();
     }
 }
---
+++ Assets/scripts/GameManger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+public class GameManger : MonoBehaviour {
+    public int Score;
+
+    public void ScoreIncrement() {
+        Score++;
+    }
+
+    public void DeathState() {
+        print("You're dead");
+    }
+}
