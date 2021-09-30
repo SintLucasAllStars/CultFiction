e513f745c108d0fc1ebd96c1e35f9538dd8e3b34
esg <jelle.bekkers@live.nl>
2021-09-30 16:05:51 +0200

    Added Health System
    + Added heart sprites
    + Added health script
    + Added invulnerability timer

--- WhipGame/Assets/Scripts/Player/Health.cs
+++ WhipGame/Assets/Scripts/Player/Health.cs
@@ -1,18 +1,57 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] public int Hp;
+    public int numhearts;
 
-    // Update is called once per frame
+    public Image[] hearts;
+    public Sprite fullheart;
+    public Sprite emptyyheart;
+    private float invTimer;
+
     void Update()
     {
-        
+        if (invTimer >= -1) {
+            invTimer -= Time.deltaTime;
+        }
+
+        //hp controller and hearts display
+        if (Hp > numhearts)
+        {
+            Hp = numhearts;
+        }
+
+        for (int i = 0; i < hearts.Length; i++)
+        {
+
+            if (i < Hp)
+            {
+                hearts[i].sprite = fullheart;
+            }
+            else
+            {
+                hearts[i].sprite = emptyyheart;
+            }
+
+            if (i < numhearts)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+        }
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.gameObject.CompareTag("Enemy") && invTimer <= 0)
+        {
+            invTimer = 2;
+            Hp--;
+        }
     }
 }
