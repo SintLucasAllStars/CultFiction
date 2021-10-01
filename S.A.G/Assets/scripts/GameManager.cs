a96971958efcef63b42db25760820b7c7ed41d0c
ArcusWithak <m.daamen@sintlucasedu.nl>
2021-10-01 10:20:18 +0200

    fixed last minute bug

--- S.A.G/Assets/scripts/GameManager.cs
+++ S.A.G/Assets/scripts/GameManager.cs
@@ -63,6 +63,8 @@ public class GameManager : MonoBehaviour
         killCount++;
         if(killCount >= planeAmountInLevel)
         {
+            killCount = 0;
+            score = 0;
             SceneManager.LoadScene("MainScreen");
         }
     }
